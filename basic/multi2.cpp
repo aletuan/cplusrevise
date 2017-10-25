3cf724f80e2aa821067dc6689eb58e09557d3e6a
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-25 15:58:47 +0900

    adding header file

---
+++ basic/multi2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "multi1.h"
+
+//int add(int x, int y); // using function declaration
+
+// using command
+// g++ multi1.cpp multi2.cpp -o multi
+int main()
+{
+   std::cout << "The sum of 3 and 4 is: " << add(3, 4) << std::endl;
+   return 0;
+}
