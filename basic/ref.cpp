ead8443a8f4f60ad716b9187cfc3b42ccc3a3de3
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-25 17:06:39 +0900

    reduce for more simple

--- basic/ref.cpp
+++ basic/ref.cpp
@@ -4,7 +4,8 @@
 int main()
 {
     int x = 5;
-    int *ptr = &x;    
+    int *ptr = &x;
+    int &ref = x;
 
     std::cout << x << "\n";
 
@@ -15,10 +16,18 @@ int main()
     // dereferencing pointer
     std::cout << *&x << "\n";
     std::cout << *ptr << "\n";
+
+    // print reference
+    std::cout << "This is reference value" << "\n";
+    std::cout << ref << std::endl;
+
+    // change reference value
+    ref = 10;
     std::cout << x << "\n";
 
     std::cout << "Type of &x " << std::endl;
     std::cout << typeid(&x).name() << "\n";
     
+
     return 0;
 }
