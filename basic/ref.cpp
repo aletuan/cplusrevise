c082f0a3f7dd2759312338d1005ae2dbad8182ce
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-25 17:13:45 +0900

    update reference sample (const ref)

--- basic/ref.cpp
+++ basic/ref.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 #include <typeinfo>
 
+/*
+void changeValueFalse(const int &ref)
+{
+    ref = 10;
+}
+*/
+
 int main()
 {
-    int x = 5;
-    int *ptr = &x;
-    int &ref = x;
+    const int x = 5;    
+    const int &ref = x;
 
     std::cout << x << "\n";
 
     // this, NOT, return address, but a POINTER
     std::cout << &x << "\n";
-    std::cout << ptr << "\n";
     
     // dereferencing pointer
     std::cout << *&x << "\n";
-    std::cout << *ptr << "\n";
 
     // print reference
     std::cout << "This is reference value" << "\n";
     std::cout << ref << std::endl;
-
-    // change reference value
-    ref = 10;
-    std::cout << x << "\n";
-
-    std::cout << "Type of &x " << std::endl;
-    std::cout << typeid(&x).name() << "\n";
-    
 
     return 0;
 }
