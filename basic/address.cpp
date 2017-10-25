fc1e0d67fd5b8c18c51cc8c5c6c961dfdf94e8b0
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-25 16:18:12 +0900

    adding dereferencing and pointer type

--- basic/address.cpp
+++ basic/address.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <typeinfo>
 
 int main()
 {
     int x = 5;
+    int *ptr = &x;
 
     std::cout << x << "\n";
+
+    // this, NOT, return address, but a POINTER
     std::cout << &x << "\n";
+    std::cout << ptr << "\n";
+    
+    // dereferencing pointer
     std::cout << *&x << "\n";
+    std::cout << *ptr << "\n";
+
+    std::cout << "Type of &x " << std::endl;
+    std::cout << typeid(&x).name() << "\n";
+    
 
     return 0;
 }
