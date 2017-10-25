3324af12fa58516b197dd5bbdf236b67cb714b2a
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-25 16:44:49 +0900

    demo for reference

--- basic/address.cpp
+++ basic/address.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int x = 5;
     int *ptr = &x;
+    int &ref = x;
 
     std::cout << x << "\n";
 
@@ -15,6 +16,10 @@ int main()
     // dereferencing pointer
     std::cout << *&x << "\n";
     std::cout << *ptr << "\n";
+
+    // print reference
+    std::cout << "This is reference value" << "\n";
+    std::cout << ref << std::endl;
 
     std::cout << "Type of &x " << std::endl;
     std::cout << typeid(&x).name() << "\n";
